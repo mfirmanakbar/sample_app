82cc244621142ca17cf6e5b4562f03933b819d4d
M Firman Akbar <em.firman.akbar@gmail.com>
2020-08-25 21:08:23 +0700

    create Users Controller New for Sign-Up

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,10 @@ Rails.application.routes.draw do
   # get '/help', to: 'static_pages#help' # --> www.example.com/help
 
   root 'static_pages#home'
+
   get '/help', to: 'static_pages#help'
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
+
+  get '/signup', to: 'users#new'
 end
