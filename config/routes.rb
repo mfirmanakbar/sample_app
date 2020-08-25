ec1ebcf5d9fd9a52a15f767e2606184a2e75e50f
M Firman Akbar <em.firman.akbar@gmail.com>
2020-08-25 19:59:53 +0700

    change url and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+
+  # EXAMPLE:
+  # get 'static_pages/help' # --> www.example.com/static_pages/help
+  # get '/help', to: 'static_pages#help' # --> www.example.com/help
+
   root 'static_pages#home'
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about'
+  get '/help', to: 'static_pages#help'
+  get '/about', to: 'static_pages#about'
+  get '/contact', to: 'static_pages#contact'
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -3,31 +3,32 @@ require 'test_helper'
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 
   # setup is a function which automatically run before every test
+
   def setup
     @base_title = 'Ruby on Rails Tutorial Sample App'
   end
 
-  test 'should get home as the root' do
-    get static_pages_home_url
-    assert_response :success
-  end
-
   test 'should get home' do
-    get static_pages_home_url
+    get root_path
     assert_response :success
     assert_select 'title', "Home | #{@base_title}"
   end
 
   test 'should get help' do
-    get static_pages_help_url
+    get help_path
     assert_response :success
     assert_select 'title', "Help | #{@base_title}"
   end
 
   test 'should get about' do
-    get static_pages_about_url
+    get about_path
     assert_response :success
     assert_select 'title', @base_title
   end
 
+  test 'should get contact' do
+    get contact_path
+    assert_response :success
+    assert_select 'title', "Contact | #{@base_title}"
+  end
 end
