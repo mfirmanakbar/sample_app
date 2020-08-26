d293f16f8c0b08e69f0e3e299debe545ae383b62
M Firman Akbar <em.firman.akbar@gmail.com>
2020-08-26 19:49:47 +0700

    add bcrypt and password_digest that will generate password_digest in DB dan attributes are password & password_confirmation

--- Gemfile
+++ Gemfile
@@ -3,14 +3,15 @@
 source 'https://rubygems.org'
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
+gem 'bcrypt', '3.1.13'
 gem 'bootsnap', '1.4.5', require: false
+gem 'bootstrap-sass', '3.4.1'
 gem 'jbuilder', '2.9.1'
 gem 'puma', '3.12.2'
 gem 'rails', '6.0.2.1'
 gem 'sass-rails', '5.1.0'
 gem 'turbolinks', '5.2.0'
 gem 'webpacker', '4.0.7'
-gem 'bootstrap-sass', '3.4.1'
 
 group :development, :test do
   gem 'byebug', '11.0.1', platforms: %i[mri mingw x64_mingw]
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,18 +1,28 @@
 class User < ApplicationRecord
 
-  # also can write like this validates(:name, presence: true)
-  # go to https://rubular.com for REGEX
-  # uniqueness: { case_sensitive: false }
-  # : false is like equalsIgnoreCase
-  # self refers to the current user
+  # 1. also can write like this validates(:name, presence: true)
+  # 2. go to https://rubular.com for REGEX
+  # 3. uniqueness: { case_sensitive: false }
+  #    -> false is like equalsIgnoreCase
+  # 4. 'self' -> refers to the current user
+  # 5. password_digest -> installed bcrypt (Store in Database)
+  # 6. has_secure_password
+  #    -> have password and password_confirmation attributes
+  # 7. Rails Console ->
+  #    user = User.find(1)
+  #    user.password_digest -> will show hash password
+  #    user.authenticate('wrong_password') ->  return FALSE
+  #    user.authenticate('correct_password') -> return User
+  #    !!user.authenticate('correct_password') -> return TRUE
+  #    `!!` -> convert object to boolean value
 
   before_save { self.email = email.downcase }
-
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-
   validates :name,  presence: true, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: true
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }
 
 end
